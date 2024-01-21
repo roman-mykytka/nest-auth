2984a01784935243ccd36a1e71f204ee81346e96
roman-mykytka <roman.mykytka@binary-studio.com>
2024-01-21 14:07:23 +0200

    NEST-AUTH-2: + added database env

--- src/config/environment/enums/environment-validation-message.ts
+++ src/config/environment/enums/environment-validation-message.ts
@@ -1,6 +1,19 @@
 const EnvironmentValidationMessage = {
-  PORT_NUMBER: 'Port must be a number',
-  PORT_REQUIRE: 'Port is required',
+  PORT_NUMBER: '{{#label}}: Port must be a number',
+  PORT_REQUIRE: '{{#label}}: Port is required',
+  DB_TYPE_STRING: '{{#label}}: Database type must be a string',
+  DB_TYPE_REQUIRE: '{{#label}}: Database type is required',
+  DB_HOST_STRING: '{{#label}}: Database host must be a string',
+  DB_HOST_REQUIRED: '{{#label}}: Database host is required',
+  DB_HOST_IP: '{{#label}}: Database host must be a valid IP address',
+  DB_PORT_NUMBER: '{{#label}}: Database port must be a number',
+  DB_PORT_REQUIRED: '{{#label}}: Database port is required',
+  DB_USERNAME_STRING: '{{#label}}: Database username must be a string',
+  DB_USERNAME_REQUIRED: '{{#label}}: Database username is required',
+  DB_PASSWORD_STRING: '{{#label}}: Database password must be a string',
+  DB_PASSWORD_REQUIRED: '{{#label}}: Database password is required',
+  DB_NAME_STRING: '{{#label}}: Database name must be a string',
+  DB_NAME_REQUIRED: '{{#label}}: Database name is required',
 };
 
 export { EnvironmentValidationMessage };
--- src/config/environment/validation-schemas/environment.validation-schema.ts
+++ src/config/environment/validation-schemas/environment.validation-schema.ts
@@ -2,11 +2,37 @@ import * as Joi from 'joi';
 import { EnvironmentValidationMessage } from '../enums/enums';
 const environmentValidationSchema = Joi.object({
   NODE_ENV: Joi.string()
+    .required()
     .valid('development', 'production', 'test')
     .default('development'),
-  PORT: Joi.number().required().default(3000).messages({
+  PORT: Joi.number().required().messages({
     'number.base': EnvironmentValidationMessage.PORT_NUMBER,
     'any.required': EnvironmentValidationMessage.PORT_REQUIRE,
+  }),
+  DB_TYPE: Joi.string().required().messages({
+    'string.base': EnvironmentValidationMessage.DB_TYPE_STRING,
+    'any.required': EnvironmentValidationMessage.DB_TYPE_REQUIRE,
+  }),
+  DB_HOST: Joi.string().required().ip().messages({
+    'string.base': EnvironmentValidationMessage.DB_HOST_REQUIRED,
+    'any.required': EnvironmentValidationMessage.DB_HOST_REQUIRED,
+    'string.ip': EnvironmentValidationMessage.DB_HOST_IP,
+  }),
+  DB_PORT: Joi.number().required().messages({
+    'number.base': EnvironmentValidationMessage.DB_PORT_REQUIRED,
+    'any.required': EnvironmentValidationMessage.DB_PORT_NUMBER,
+  }),
+  DB_USERNAME: Joi.string().required().messages({
+    'string.base': EnvironmentValidationMessage.DB_USERNAME_STRING,
+    'any.required': EnvironmentValidationMessage.DB_USERNAME_REQUIRED,
+  }),
+  DB_PASSWORD: Joi.string().required().messages({
+    'string.base': EnvironmentValidationMessage.DB_PASSWORD_STRING,
+    'any.required': EnvironmentValidationMessage.DB_PASSWORD_REQUIRED,
+  }),
+  DB_NAME: Joi.string().required().messages({
+    'string.base': EnvironmentValidationMessage.DB_NAME_STRING,
+    'any.required': EnvironmentValidationMessage.DB_NAME_REQUIRED,
   }),
 });
 
